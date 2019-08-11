ec245b1ce171ddc153e05e24ebd5e2d9ec269233
lomelina_etro <lomelina@etro.vub.ac.be>
2019-08-11 19:45:13 +0200

    Vector3 extended by a weight

--- sources/src/be/etrovub/c3d4java/parameters/Vector3.java
+++ sources/src/be/etrovub/c3d4java/parameters/Vector3.java
@@ -2,12 +2,17 @@ package be.etrovub.c3d4java.parameters;
 
 public class Vector3 {
 
-	 public Vector3(float x, float y, float z)
+	public Vector3(float x, float y, float z, float w)
     {
         this.X = x;
         this.Y = y;
         this.Z = z;
+        this.W = w;
     }
+	public Vector3(float x, float y, float z)
+	{
+		this(x,y,z,0);
+	}
 
-    public float X, Y, Z;
+    public float X, Y, Z, W;
 }
